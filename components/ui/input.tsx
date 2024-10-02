61a9913e2db573a69635633d7076266126a31a2e
Yamasaitensai <yamasaitensai@gmail.com>
2024-10-02 17:18:26 +0900

    2nd

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,10 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// InputProps インターフェースを削除し、直接 React.InputHTMLAttributes<HTMLInputElement> を使用
+// export interface InputProps
+//   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
